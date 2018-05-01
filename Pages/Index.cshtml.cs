4ae9ff2e2cf8e33db09052f8324d6b123f4532ae
dan-romero-mancia <daniel.romeromancia@edu.sait.ca>
2018-04-30 22:00:55 -0600

    Added logo and style changes.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using GundamRazor.Data;
+using GundamRazor.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GundamRazor.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly GundamContext context; 
+        public ModelKit[] PopularModelKits;
+
+        public IndexModel(GundamContext context) {
+            this.context = context;
+        }
         public void OnGet()
         {
-
+            // Only done for testing, change later
+            PopularModelKits = context.ModelKits.Where(m => m.Grade == ModelKit.GradeEnum.MG)
+                .Include(m => m.MobileSuit)
+                .ToArray();
         }
     }
 }
